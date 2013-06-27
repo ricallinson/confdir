f8bf0a2bf08a7837a905ae5fc203460f46e22939
Ric Allinson <capecodehq@yahoo.co.uk>
2013-06-27 13:46:56 -0700

    Added basic tests to get code coverage

--- test/index.js
+++ test/index.js
@@ -26,10 +26,54 @@
 "use strict";
 
 var confdir = require("../"),
-    assert = require("assert");
+    assert = require("assert"),
+    pathlib = require("path");
 
-describe("confdir", function () {
+describe("confdir.read()", function () {
+
     it("should return function", function () {
         assert.equal(typeof confdir.read, "function");
+    });
+
+    it("should return an empty object", function () {
+
+        var config = confdir.read(),
+            name,
+            test = false;
+
+        for (name in config) {
+            test = true;
+        }
+
+        assert.strictEqual(test, false);
+    });
+
+    it("should return jsonValue, yamlValue, ymlValue", function () {
+
+        var config = confdir.read(pathlib.join(__dirname, "fixtures"));
+
+        assert.equal(config.json.jsonKey, "jsonValue");
+        assert.equal(config.yaml.yamlKey, "yamlValue");
+        assert.equal(config.yml.ymlKey, "ymlValue");
+    });
+
+    it("should return jsonSubValue, yamlSubValue, ymlSubValue", function () {
+
+        var config = confdir.read(pathlib.join(__dirname, "fixtures", "subfolder"));
+
+        assert.equal(config.subjson.jsonSubKey, "jsonSubValue");
+        assert.equal(config.subyaml.yamlSubKey, "yamlSubValue");
+        assert.equal(config.subyml.ymlSubKey, "ymlSubValue");
+    });
+
+    it("should return 1, 2, mapValue", function () {
+
+        var config = confdir.read(pathlib.join(__dirname, "fixtures"), pathlib.join(__dirname, "fixtures", "defaults"));
+
+        assert.equal(config.json.jsonKey, "jsonValue");
+        assert.equal(config.yaml.yamlKey, 1);
+        assert.equal(config.yaml.yamlDefault, 2);
+        assert.equal(config.yaml.yamlMap.mapKey, "mapValue");
+        assert.equal(config.yml.ymlKey, "ymlValue");
     });
 });
